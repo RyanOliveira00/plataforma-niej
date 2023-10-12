fc2fbd42ab359409188dff383ea1affb1da21bf5
Ryan Oliveira <jryancoelho79@hotmail.com>
2023-10-12 18:51:56 -0300

    feat:

--- src/components/layouts/RecordLayout.tsx
+++ src/components/layouts/RecordLayout.tsx
@@ -1,4 +1,6 @@
 import { type ReactNode } from "react";
+import { Navbar } from "../modules/Navbar";
+import { Sidebar } from "../modules/Sidebar";
 
 type RecordLayoutProps = {
   children: ReactNode;
@@ -7,6 +9,8 @@ type RecordLayoutProps = {
 export function RecordLayout({ children }: RecordLayoutProps) {
   return (
     <div className="flex flex-col bg-gray-100">
+      <Navbar />
+      <Sidebar />
       <main className="flex min-h-[calc(100vh_-_4rem)] flex-col items-center justify-center px-2 md:min-h-[calc(100vh_-_5rem)]">
         {children}
       </main>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,15 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 
+import { AppProvider } from "@/contexts/AppProvider";
 import "@/styles/globals.css";
+import "react-toastify/dist/ReactToastify.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <AppProvider>
+      <Component {...pageProps} />
+    </AppProvider>
+  );
 };
 
 export default MyApp;
